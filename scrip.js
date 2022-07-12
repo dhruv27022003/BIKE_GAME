55b5189bcb1aa0107c003229022781a97bac6db5
DHRUV DADHEECH <dhruvdadheech@DHRUVs-MacBook-Air.local>
2022-07-12 14:43:13 +0530

    first commit

---
+++ scrip.js
@@ -0,0 +1,20 @@
+
+
+audio = new Audio('media/sports.mp3');
+
+  setTimeout(() => {
+    audio.play()
+}, 1000);
+
+
+audio1 = new Audio('media/tok.mp3');
+audio2 = new Audio('media/Kawa.mp3');
+
+audio1.play()
+
+setTimeout(() => {
+    audio2.play()
+}, 1000);
+
+//   setTimeout(() => {
+// }, 700);
---
+++ scripp.js
@@ -0,0 +1,17 @@
+
+
+audio = new Audio('media/kawa.mp3');
+
+setTimeout(() => {
+  audio.play()
+}, 1);
+
+
+audio1 = new Audio('media/tok.mp3');
+
+
+audio1.play()
+
+setTimeout(() => {
+  audio.play()
+}, 6000);
